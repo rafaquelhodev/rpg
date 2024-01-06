16d6278e3e3d09528192c9c064c70172fcdb174e
Rafael Macedo <rafael@Rafaels-MacBook-Pro.local>
2024-01-06 19:34:24 -0300

    feat: combat system

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,14 +1,30 @@
 extends CharacterBody2D
 
-var speed = 40
+var speed = 50
 var player_chase = false
 var player = null
+const IS_ENEMY = true
+var player_in_attack_range = null
+var attack_cooldown = true
+var health = 100
+const DAMAGE = 5
+var alive = true
+
+signal enemy_got_hit(amount)
 
 func _physics_process(delta: float) -> void:
 	if player:
 		position += (player.position - position)/speed
 		maybe_flip(player)
 		move_and_slide()
+		
+	attack_player()
+	
+	if health <= 0:
+		alive = false
+		health = 0
+		print("enemy is dead")
+		queue_free()
 
 func maybe_flip(player):
 	if (player.position.x - position.x) < 0:
@@ -20,13 +36,34 @@ func _on_detection_area_body_entered(body: Node2D) -> void:
 	player = body
 	player_chase = true
 	$AnimatedSprite2D.play("walk")
-	
-	
-		
-
 
 func _on_detection_area_body_exited(body: Node2D) -> void:
 	player = null
 	player_chase = false
 	$AnimatedSprite2D.play("idle")	
-	
+
+func _on_enemy_hitbox_body_entered(body: Node2D) -> void:
+	if body.get("IS_PLAYER"):
+		player_in_attack_range = body
+		
+func _on_enemy_hitbox_body_exited(body: Node2D) -> void:
+	if body.get("IS_PLAYER"):
+		player_in_attack_range = null
+
+func attack(player):
+	player.being_attacked(DAMAGE)
+
+func being_attacked(damage):
+	health -= damage
+	print("enemy health")
+	print(health)
+
+func attack_player():
+	if player_in_attack_range and attack_cooldown:
+		attack(player_in_attack_range)
+		attack_cooldown = false
+		$attack_cooldown.start()
+
+func _on_attack_cooldown_timeout() -> void:
+	attack_cooldown = true
+
--- scripts/player.gd
+++ scripts/player.gd
@@ -3,12 +3,25 @@ extends CharacterBody2D
 const SPEED = 100.0
 enum DIRECTIONS {LEFT, RIGHT, UP, DOWN, NONE}
 var current_direction = DIRECTIONS.NONE
+var enemy_in_attack_range = null
+var attack_cooldown = true
+var health = 100
+var alive = true
+const IS_PLAYER = true
+const DAMAGE = 15
 
 func _ready() -> void:
 	$AnimatedSprite2D.play("front_idle")
 
 func _physics_process(delta: float) -> void:
 	player_movement(delta)
+	attack_enemy()
+	
+	if health <= 0:
+		alive = false
+		health = 0
+		print("player is dead")
+		queue_free()
 	
 func player_movement(delta: float) -> void:
 	if Input.is_action_pressed("ui_left"):
@@ -70,3 +83,28 @@ func play_animation(is_moving):
 		else:
 			anim.play("front_idle")
 			
+			
+func _on_player_hitbox_body_entered(body: Node2D) -> void:
+	if body.get("IS_ENEMY"):
+		enemy_in_attack_range = body
+		
+func _on_player_hitbox_body_exited(body: Node2D) -> void:
+	if body.get("IS_ENEMY"):
+		enemy_in_attack_range = null
+
+func attack(player):
+	player.being_attacked(DAMAGE)
+
+func being_attacked(damage):
+	health -= damage
+	print("player health")
+	print(health)
+	
+func attack_enemy():
+	if enemy_in_attack_range and attack_cooldown:
+		attack(enemy_in_attack_range)
+		attack_cooldown = false
+		$attack_cooldown.start()
+
+func _on_attack_cooldown_timeout() -> void:
+	attack_cooldown = true
